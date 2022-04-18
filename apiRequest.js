8236588d4c92b4a323d7741fae3b87c6292c69dc
Sergio Cabral <git@sergiocabral.com>
2022-04-18 20:17:45 -0300

    Request de API com node-fetch (equivalente aos browsers)

--- apiRequest.js
+++ apiRequest.js
@@ -1,23 +1,9 @@
-const https = require('https');
+const fetch = require('node-fetch');
 
 async function apiRequest(url) {
-    return new Promise((resolve, reject) => {
-        const request = https.get(url, (response) => {
-            let data = '';
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-            response.on('end', () => {
-                resolve(JSON.parse(data));
-            });
-        });
-
-        request.on('error', (error) => {
-            reject(error);
-        });
-
-        request.end();
-    });
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
 }
 
 module.exports = apiRequest;
