4e0fefb8df386a6bd133e65d579af7a0b240c7d4
Sergio Cabral <git@sergiocabral.com>
2022-04-18 19:37:05 -0300

    Ajustado BOT para responder previsão do tempo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const tmi = require('tmi.js');
+const weather = require('./weather');
 
 // Define configuration options
 const opts = {
@@ -22,16 +23,16 @@ client.on('connected', onConnectedHandler);
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
+async function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
 
   // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
+  if (commandName.startsWith('!clima ')) {
+    const city = commandName.substring(commandName.indexOf(' ')).trim();
+    client.say(target, await weather(city));
     console.log(`* Executed ${commandName} command`);
   } else {
     console.log(`* Unknown command ${commandName}`);
---
+++ weather.js
@@ -0,0 +1,5 @@
+function weather(city) {
+    return `Não sei se está quento ou frio em ${city}`;
+}
+
+module.exports = weather;
