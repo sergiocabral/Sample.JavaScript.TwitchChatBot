242a16aab209b3aabb2cc40e2b9705892fa080cc
Sergio Cabral <git@sergiocabral.com>
2022-04-18 20:14:44 -0300

    Request de API nativo do NodeJS

--- apiRequest.js
+++ apiRequest.js
@@ -1,5 +1,23 @@
-function apiRequest(url) {
-    return null;
+const https = require('https');
+
+async function apiRequest(url) {
+    return new Promise((resolve, reject) => {
+        const request = https.get(url, (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+            response.on('end', () => {
+                resolve(JSON.parse(data));
+            });
+        });
+
+        request.on('error', (error) => {
+            reject(error);
+        });
+
+        request.end();
+    });
 }
 
 module.exports = apiRequest;
